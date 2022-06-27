46977a50918e1ac012777e4f41b1a9d5cb317ef9
Stelian Bochev <stbochev@gmail.com>
2022-06-27 20:20:24 +0100

    Finish Design

---
+++ app.js
@@ -0,0 +1,25 @@
+let scrollpos = window.scrollY;
+const header = document.querySelector("nav");
+const header_height = header.offsetHeight;
+
+const add_class_on_scroll = () => header.classList.add("fade-in");
+const remove_class_on_scroll = () => header.classList.remove("fade-in");
+
+window.addEventListener("scroll", function () {
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= header_height) {
+    add_class_on_scroll();
+  } else {
+    remove_class_on_scroll();
+  }
+});
+
+function toggle() {
+  let x = document.querySelector("#myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
